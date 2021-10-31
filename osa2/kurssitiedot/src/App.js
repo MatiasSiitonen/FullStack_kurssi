2d0a7fac3e0e327308dd148e306cbc3aab97ba77
MatiasSiitonen <v.matias.siitonen@gmail.com>
2021-10-31 12:31:02 +0200

    Tehtävä 2.2 valmis

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -6,12 +6,12 @@ const Header = ({ course }) => {
   )
 }
 
-//const Total = ({ course }) => {
-//  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-//  return (
-//    <p>Number of exercises {sum}</p>
-//  )
-//}
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return (
+    <strong>total of exercises {sum}</strong>
+  )
+}
 
 const Part = (props) => {
   return (
@@ -37,6 +37,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course = {course} />
     </div>
   )
 }
